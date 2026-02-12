240a570981c41a79744d6672275acbd9383bb50b
warnockr <warnockr@OA-S142-11>
2026-02-11 21:45:20 -0500

    Add project files.

---
+++ Assets/Scripts/Tasks/Actions/WanderTask.cs
@@ -0,0 +1,46 @@
+using NodeCanvas.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class WanderTask : ActionTask 
+	{
+		public BBParameter<float> timeSinceLastSampleBBP;
+		public BBParameter<Vector3> TargetPositionBBP;
+		public BBParameter<bool> isMovingBBP;
+		public BBParameter<float> DistanceToTree;
+		public LayerMask treeMask;
+
+		public float wanderDistance = 4f;
+		public float wanderRadius = 3f;
+
+		
+		protected override void OnUpdate() 
+		{
+
+                if (timeSinceLastSampleBBP.value == 0 && isMovingBBP.value == false)
+			{
+				Vector3 destination = CalulateTargetPosition();
+				if(NavMesh.SamplePosition(destination, out NavMeshHit hitInfo, wanderDistance + wanderRadius, NavMesh.AllAreas))
+				{
+					TargetPositionBBP.value = hitInfo.position;
+				}
+			}
+		}
+
+		private Vector3 CalulateTargetPosition()
+		{
+			Vector3 circleCenter = agent.transform.position +agent.transform.forward * wanderDistance;
+			Vector3 randomPoint = Random.insideUnitSphere.normalized * wanderRadius;  
+
+			Vector3 destination = circleCenter + randomPoint;
+
+			return destination;
+		}
+
+	}
+}
---
+++ Assets/Scripts/clickToMove.cs
@@ -0,0 +1,61 @@
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+public class clickToMove : MonoBehaviour
+{
+
+    public NavMeshAgent navAgent;
+    public LayerMask terrainLayers;
+    private InputSystem_Actions inputActions;
+    
+
+
+    //void Start()
+    //{
+    //    inputActions = new InputSystem_Actions();
+    //    inputActions.Enable();
+    //    inputActions.Player.Attack.performed += OnAtttack;
+
+    //}
+
+    //private void OnAtttack(InputAction.CallbackContext context)
+    //{
+    //    Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //    if(Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+    //    {
+    //        navAgent.SetDestination(hitInfo.point);
+    //    }
+    //}
+
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Attack.performed += OnAttack;
+    }
+
+    private void OnAttack(InputAction.CallbackContext context)
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(Physics.Raycast(mouseRay,out RaycastHit hitInfo, float.MaxValue, terrainLayers))
+        {
+            navAgent.SetDestination(hitInfo.point);
+        }
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Attack.performed -= OnAttack;
+        inputActions.Disable();
+    }
+
+}
+
